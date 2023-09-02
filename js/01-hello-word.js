0ed702565bb2aed101137bb1662658d7b6b5d055
@Nicolas_Oliveira <125362779+Nicolas-tech25@users.noreply.github.com>
2023-09-02 09:42:37 -0300

    pasta js

---
+++ js/01-hello-word.js
@@ -0,0 +1,11 @@
+/* Arquivo 01-hello-word.html */
+document.write("Hello word!");
+console.log("Hello Word!");
+
+
+
+
+
+        
+
+        
---
+++ js/03-janelas-de-dialogo.js
@@ -0,0 +1,18 @@
+/* Arquivo 03-janelas-de-dialogo.html */
+            
+        /* Fuções para janelas/caixas de diálogo.
+        
+        alert() -> janela de alerta para exibição de mensagem.
+        prompt() -> janela de entrada de dados.
+        confirm() -> janela de exibição de mensagem com duas respostas.  */
+
+        // alert();
+        // alert("VAI CORINTHIANS🦅🦅");
+
+        // prompt();
+        // let nome = prompt("Digite seu Time do coração: ");
+        // alert("🦅 Vai "+nome+"🦅");
+
+        // confirm();
+        let resposta = confirm("Deseja realmente sair?");
+        console.log(resposta);
