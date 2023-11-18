2f1da96aecad166b85becc5623c5cc15bfb5be78
@Nicolas_Oliveira <125362779+Nicolas-tech25@users.noreply.github.com>
2023-11-18 11:58:40 -0300

    vsdf

---
+++ js/13-player.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const lista = document.querySelector("#lista-de-musicas");
+const reprodutor = document.querySelector("audio");
+
+/* Monitorando o evento de mudanças de opções na lista */
+lista.addEventListener("change", function(){
+    let musicaSelecionada = lista.options[lista.selectedIndex];
+    console.log(musicaSelecionada);
+
+    reprodutor.src = musicaSelecionada.value;
+    
+    if(musicaSelecionada.value !== ""){
+        reprodutor.play();
+    }
+});
